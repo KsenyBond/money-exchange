e15b7ff41a0459449aae6b6c06894fc7815f57f6
KsenyBond <kseny.bondarchuk@gmail.com>
2019-03-22 01:03:43 +0300

    feat: money exchange

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
-// PLEASE DON'T change function name
+// // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
-}
+    var availableCoins = {
+        'H': 50,
+        'Q': 25,
+        'D': 10,
+        'N': 5,
+        'P': 1
+    },
+        necessaryCoins = {};
+
+    if (currency <= 0) {
+        necessaryCoins = {};
+    } else if (currency > 10000) {
+        necessaryCoins = {error: "You are rich, my friend! We don't have so much coins for exchange"}; 
+    } else {
+        for (key in availableCoins) {
+            var qntCoins = Math.floor(currency / availableCoins[key]);
+            if (qntCoins) {
+                necessaryCoins[key] = qntCoins;
+                currency = currency % availableCoins[key];
+            }
+        }
+    }
+    return necessaryCoins;
+};
